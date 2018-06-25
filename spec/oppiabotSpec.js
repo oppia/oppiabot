ffec8bc912ed2b45bcef9cf330efad60faee67a5
Apurv-Bajaj <apurvabajaj007@gmail.com>
2018-06-25 08:46:08 +0530

    Review changes

--- spec/oppiabotSpec.js
+++ spec/oppiabotSpec.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const {createRobot} = require('probot');
-const plugin = require('..');
+// The plugin refers to the actual app in index.js.
+const plugin = require('../index');
 const apiForSheetsModule = require('../lib/apiForSheets');
 const checkMergeConflictsModule = require('../lib/checkMergeConflicts');
 const pullRequestpayload = require('../fixtures/pullRequestPayload.json');
@@ -18,11 +19,11 @@ describe('Oppiabot\'s', () => {
         createComment: jasmine.createSpy('createComment')
       }
     };
-    robot.auth = () => Promise.resolve(github);
+      robot.auth = () => Promise.resolve(github);
     done();
   });
 
-  describe('apiForSheets function', () => {
+  describe('apiForSheets', () => {
     beforeEach(function (done) {
       spyOn(apiForSheetsModule, 'apiForSheets').and.callThrough();
       spyOn(apiForSheetsModule, 'authorize').and.callThrough();
@@ -31,53 +32,53 @@ describe('Oppiabot\'s', () => {
       done();
     });
 
-    it('is called', () => {
+    it('should be called', () => {
       expect(apiForSheetsModule.apiForSheets).toHaveBeenCalled();
     });
 
-    it('is called once for the given payload', () => {
+    it('should be called once for the given payload', () => {
       expect(apiForSheetsModule.apiForSheets.calls.count()).toEqual(1);
     });
 
-    it('is called with three arguments for the given payload', () => {
+    it('should be called with three arguments for the given payload', () => {
       expect(
         apiForSheetsModule.apiForSheets.calls.argsFor(0).length).toEqual(3);
     });
 
-    it('is called with the correct username for the given payload', () => {
+    it('should be called with the correct username for the given payload', () => {
       expect(
         apiForSheetsModule.apiForSheets.calls.argsFor(0)[0])
         .toEqual('testuser7777');
     });
 
-    it('is called for a pull request for the given payload', () => {
+    it('should be called for a pull request for the given payload', () => {
       expect(
         apiForSheetsModule.apiForSheets.calls.argsFor(0)[2])
         .toEqual(true);
     });
 
-    it('calls the authorize function', () => {
+    it('should call authorize', () => {
       expect(apiForSheetsModule.authorize).toHaveBeenCalled();
     });
 
-    it('calls the authorize function once', () => {
+    it('should call authorize once', () => {
       expect(apiForSheetsModule.authorize.calls.count()).toEqual(1);
     });
 
-    it('calls the authorize function with two arguments', () => {
+    it('should call authorize with two arguments', () => {
       expect(
         apiForSheetsModule.authorize.calls.argsFor(0).length).toEqual(2);
     });
 
-    it('further calls the checkClaSheet function', () => {
+    it('should further call checkClaSheet', () => {
       expect(apiForSheetsModule.checkClaSheet).toHaveBeenCalled();
     });
 
-    it('further calls the checkClaSheet function once', () => {
+    it('should further call checkClaSheet', () => {
       expect(apiForSheetsModule.checkClaSheet.calls.count()).toEqual(1);
     });
 
-    it('further calls the checkClaSheet function with one argument', () => {
+    it('should further call checkClaSheet with one argument', () => {
       expect(
         apiForSheetsModule.checkClaSheet.calls.argsFor(0).length).toEqual(1);
     });
