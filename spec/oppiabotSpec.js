ec534614d576d99e4deba7e98d2aaa41fd349ba6
Apurv-Bajaj <apurvabajaj007@gmail.com>
2018-06-26 14:41:14 +0530

    Add more tests and modularity

--- spec/oppiabotSpec.js
+++ spec/oppiabotSpec.js
@@ -17,7 +17,66 @@ describe('Oppiabot\'s', () => {
 
     github = {
       issues: {
-        createComment: jasmine.createSpy('createComment')
+        addLabels: jasmine.createSpy('addLabels').and.returnValue({
+          params: {
+            number: 5139,
+            owner: 'oppia',
+            repo: 'oppia',
+            labels: [
+              "PR: don't merge - NEEDS CLA"
+            ]
+          }
+        }),
+        createComment: jasmine.createSpy('createComment').and.returnValue({
+          params: {
+            number: 5139,
+            owner: 'oppia',
+            repo: 'oppia',
+            body: 'Hi! @tester7777. Welcome to Oppia! ' +
+              'Please could you follow the instructions' +
+              '<a href=\"https://github.com/oppia/oppia/wiki/Contributing-code-to-Oppia#setting-things-up\">here</a>' +
+              "to get started ? You'll need to do this before" +
+              'we can accept your PR. Thanks!'
+          }
+        }),
+        getIssueLabels: jasmine.createSpy('getIssueLabels').and.returnValue({
+          data:
+            [
+              {
+                id: 248679580,
+                node_id: 'MDU6TGFiZWwyNDg2Nzk1ODA=',
+                url: 'https://api.github.com/repos/oppia/oppia/labels/PR:%20LGTM',
+                name: 'PR: LGTM',
+                color: '009800',
+                'default': false
+              },
+              {
+                id: 971968901,
+                node_id: 'MDU6TGFiZWw5NzE5Njg5MDE=',
+                url: "https://api.github.com/repos/oppia/oppia/labels/PR:%20don't%20merge%20-%20HAS%20MERGE%20CONFLICTS",
+                name: "PR: don't merge - HAS MERGE CONFLICTS",
+                color: 'd93f0b',
+                'default': false
+              },
+              {
+                id: 638838928,
+                node_id: 'MDU6TGFiZWw2Mzg4Mzg5Mjg=',
+                url: 'https://api.github.com/repos/oppia/oppia/labels/PR:%20for%20current%20release',
+                name: 'PR: for current release',
+                color: 'FF69B4',
+                'default': false
+              },
+              {
+                id: 638839900,
+                node_id: 'MDU6TGFiZWw2Mzg4Mzk5MDA=',
+                url: 'https://api.github.com/repos/oppia/oppia/labels/PR:%20released',
+                name: 'PR: released',
+                color: '00FF00',
+                'default': false
+              }
+            ]
+        }),
+        removeLabel: jasmine.createSpy('removeLabel')
       }
     };
     robot.auth = () => Promise.resolve(github);
@@ -29,7 +88,6 @@ describe('Oppiabot\'s', () => {
       spyOn(apiForSheetsModule, 'apiForSheets').and.callThrough();
       spyOn(apiForSheetsModule, 'authorize').and.callThrough();
       spyOn(apiForSheetsModule, 'checkClaSheet').and.callThrough();
-      spyOn(apiForSheetsModule, 'generateOutput').and.callThrough();
       robot.receive(pullRequestpayload);
       done();
     });
@@ -47,7 +105,8 @@ describe('Oppiabot\'s', () => {
         apiForSheetsModule.apiForSheets.calls.argsFor(0).length).toEqual(3);
     });
 
-    it('should be called with the correct username for the given payload', () => {
+    it('should be called with the' +
+    'correct username for the given payload', () => {
       expect(
         apiForSheetsModule.apiForSheets.calls.argsFor(0)[0])
         .toEqual('testuser7777');
@@ -76,7 +135,7 @@ describe('Oppiabot\'s', () => {
       expect(apiForSheetsModule.checkClaSheet).toHaveBeenCalled();
     });
 
-    it('should further call checkClaSheet', () => {
+    it('should further call checkClaSheet once', () => {
       expect(apiForSheetsModule.checkClaSheet.calls.count()).toEqual(1);
     });
 
@@ -85,19 +144,30 @@ describe('Oppiabot\'s', () => {
         apiForSheetsModule.checkClaSheet.calls.argsFor(0).length).toEqual(1);
     });
 
-    it('should further call generateOutput', () => {
+    it('should not remove any label for the given payload', async () => {
+      var commentCreatedStatus = await apiForSheetsModule.generateOutput(
+        [['apb7'], ['kevinlee12']]);
       expect(
-        apiForSheetsModule.generateOutput).toHaveBeenCalled();
+        github.issues.getIssueLabels).toHaveBeenCalled();
+      expect(
+        github.issues.removeLabel).not.toHaveBeenCalled();
     });
 
-    it('should further call checkClaSheet with two arguments', () => {
+    it('should add a relevant label for the given payload', async () => {
+      var commentCreatedStatus = await apiForSheetsModule.generateOutput(
+        [['apb7'], ['kevinlee12']]);
       expect(
-        apiForSheetsModule.generateOutput.calls.argsFor(0).length).toEqual(2);
+        github.issues.getIssueLabels).toHaveBeenCalled();
+      expect(
+        github.issues.addLabels).toHaveBeenCalled();
     });
 
-    it('should post a comment for the given payload', () => {
+    it('should post a comment for the given payload', async () => {
+      var commentCreatedStatus = await apiForSheetsModule.generateOutput(
+        [['apb7'], ['kevinlee12']]);
       expect(
         github.issues.createComment).toHaveBeenCalled();
+      expect(commentCreatedStatus).toEqual(true);
     });
   });
 });
