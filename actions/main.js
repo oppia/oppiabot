2e028c6c93cc5258de40a746df402261885f3952
James James <jamesjay4199@gmail.com>
2020-05-27 10:25:24 +0100

    testing hooks

--- actions/main.js
+++ actions/main.js
@@ -17,8 +17,11 @@
  */
 
 const core = require('@actions/core');
-const {context} = require('@actions/github');
+const { context } = require('@actions/github');
 const dispatcher = require('./src/dispatcher');
 
-core.info(`About to dispatch:${context.eventName} and ${context.payload.action}.`);
+console.log('Testing hooks');
+core.info(
+  `About to dispatch:${context.eventName} and ${context.payload.action}.`
+);
 dispatcher.dispatch(context.eventName, context.payload.action);
