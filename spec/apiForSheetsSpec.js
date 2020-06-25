f653d7e7c7101cce49ba6f7e7c865971bcf78396
Ankita Saxena <ankitasonu24@gmail.com>
2020-06-25 22:21:42 +0530

    Update cla check to test all commits

--- spec/apiForSheetsSpec.js
+++ spec/apiForSheetsSpec.js
@@ -9,8 +9,9 @@ const checkPullRequestBranchModule = require('../lib/checkPullRequestBranch');
 const checkWipModule = require('../lib/checkWipDraftPR');
 const scheduler = require('../lib/scheduler');
 const pullRequestPayload = JSON.parse(
-  JSON.stringify(require('../fixtures/pullRequestPayload.json'))
-);
+  JSON.stringify(require('../fixtures/pullRequestPayload.json')));
+const commitsData = JSON.parse(
+  JSON.stringify(require('../fixtures/commits.json')));
 const checkPullRequestJobModule = require('../lib/checkPullRequestJob');
 const {google} = require('googleapis');
 const {OAuth2Client} = require('google-auth-library');
@@ -52,6 +53,11 @@ describe('Api For Sheets Module', () => {
           },
         }),
         update: jasmine.createSpy('update').and.resolveTo({}),
+      },
+      pulls: {
+        listCommits: jasmine.createSpy('listCommits').and.returnValue({
+          data: commitsData
+        }),
       },
     };
 
@@ -204,15 +210,50 @@ describe('Api For Sheets Module', () => {
       });
     });
 
-    it('should not close the PR if user has signed cla', async () => {
+    it('should not close the PR if all users have signed cla', async () => {
+      // Add username to CLA sheet.
+      await apiForSheetsModule.generateOutput(
+        [...claData, ['testuser7777'], ['testuser7778']],
+        pullRequestPayload.payload.pull_request.number,
+        pullRequestPayload.payload.pull_request
+      );
+      expect(github.issues.createComment).not.toHaveBeenCalled();
+      expect(github.issues.update).not.toHaveBeenCalled();
+    });
+
+    it('should close the PR if atleast one user has not signed cla', async () => {
       // Add username to CLA sheet.
       await apiForSheetsModule.generateOutput(
         [...claData, ['testuser7777']],
         pullRequestPayload.payload.pull_request.number,
         pullRequestPayload.payload.pull_request
       );
-      expect(github.issues.createComment).not.toHaveBeenCalled();
-      expect(github.issues.update).not.toHaveBeenCalled();
+
+      var linkText = 'here';
+      var linkResult = linkText.link(
+        'https://github.com/oppia/oppia/wiki/Contributing-code-to-' +
+        'Oppia#setting-things-up'
+      );
+      expect(github.issues.createComment).toHaveBeenCalledWith({
+        owner: pullRequestPayload.payload.repository.owner.login,
+        repo: pullRequestPayload.payload.repository.name,
+        number: pullRequestPayload.payload.pull_request.number,
+        body:
+          'Hi! @testuser7778,' +
+          ' Welcome to Oppia! Please could you ' +
+          'follow the instructions ' +
+          linkResult +
+          " to get started? You'll need to do " +
+          'this before we can accept your PR. I am closing this PR for now. ' +
+          'Feel free to re-open it once you are done with the above ' +
+          'instructions. Thanks!',
+      });
+      expect(github.issues.update).toHaveBeenCalledWith({
+        issue_number: pullRequestPayload.payload.pull_request.number,
+        owner: pullRequestPayload.payload.repository.owner.login,
+        repo: pullRequestPayload.payload.repository.name,
+        state: 'closed',
+      });
     });
 
     it('should do nothing if no data is obtained from sheet', async () => {
