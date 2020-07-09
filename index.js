221c50dff7ed70bb2077055d09ce7a1444705867
James James <jamesjay4199@gmail.com>
2020-07-09 13:36:43 +0100

    Setup force push check (#101)
    * complete force push check
    
    * write tests
    
    * address review comments
    
    * address review comments

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const checkPullRequestLabelsModule = require('./lib/checkPullRequestLabels');
 const checkPullRequestBranchModule = require('./lib/checkPullRequestBranch');
 const checkWipModule = require('./lib/checkWipDraftPR');
 const checkPullRequestJobModule = require('./lib/checkPullRequestJob');
+const checkBranchPushModule = require('./lib/checkBranchPush');
 
 const constants = require('./constants');
 const checkIssueAssigneeModule = require('./lib/checkIssueAssignee');
@@ -55,6 +56,9 @@ const runChecks = async (context, checkEvent) => {
           case constants.issuesAssignedCheck:
             await checkIssueAssigneeModule.checkAssignees(context);
             break;
+          case constants.forcePushCheck:
+            await checkBranchPushModule.handleForcePush(context);
+            break;
         }
       }
     }
@@ -101,7 +105,7 @@ module.exports = (oppiabot) => {
 
   oppiabot.on('pull_request.labeled', async (context) => {
     if (checkWhitelistedAccounts(context)) {
-      await runChecks(context, constants.labelEvent);
+      await runChecks(context, constants.PRLabelEvent);
     }
   });
 
@@ -132,6 +136,14 @@ module.exports = (oppiabot) => {
       // eslint-disable-next-line no-console
       console.log('A PR HAS BEEN EDITED...');
       await runChecks(context, constants.editEvent);
+    }
+  });
+
+  oppiabot.on('push', async (context) => {
+    if (checkWhitelistedAccounts(context)) {
+      // eslint-disable-next-line no-console
+      console.log('A BRANCH HAS BEEN PUSHED...');
+      await runChecks(context, constants.pushEvent);
     }
   });
 };
