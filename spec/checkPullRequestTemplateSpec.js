0bff000a5a0d6416465f2e4fb020fad6aa0a5d34
Praneeth <praneethg2001@gmail.com>
2021-02-18 17:27:40 +0530

    added frontend tests

--- spec/checkPullRequestTemplateSpec.js
+++ spec/checkPullRequestTemplateSpec.js
@@ -52,6 +52,7 @@ describe('Pull Request Template', () => {
     'lets reviewers restart your CircleCI tests for you.\r\n' +
     "- [ ] The PR is made from a branch that's **not** called " +
     '"develop".\r\n\r\n' +
+    '## Proof that changes are correct\r\n\r\n' +
     '## PR Pointers\r\n\r\n' +
     '- Oppiabot will ' +
     "notify you when you don't add a PR_CHANGELOG label.";
@@ -70,6 +71,7 @@ describe('Pull Request Template', () => {
     'lets reviewers restart your CircleCI tests for you.\r\n' +
     "- [ ] The PR is made from a branch that's **not** called " +
     '"develop".\r\n\r\n' +
+    '## Proof that changes are correct\r\n\r\n' +
     '## PR Pointers\r\n\r\n' +
     '- Oppiabot will ' +
     "notify you when you don't add a PR_CHANGELOG label.";
@@ -79,6 +81,29 @@ describe('Pull Request Template', () => {
     '1. This PR fixes or fixes part of #[fill_in_number_here].\r\n' +
     '2. This PR does the following: [Explain here what your PR ' +
     'does and why]\r\n\r\n' +
+    '## Proof that changes are correct\r\n\r\n' +
+    '## PR Pointers\r\n\r\n' +
+    '- Oppiabot will ' +
+    "notify you when you don't add a PR_CHANGELOG label.";
+
+  const bodyWithNoProofOfChangesSection =
+  '## Overview\r\n' +
+    '1. This PR fixes or fixes part of #[fill_in_number_here].\r\n' +
+    '2. This PR does the following: [Explain here what your PR ' +
+    'does and why]\r\n\r\n' +
+    '## Essential Checklist\r\n\r\n' +
+    '- [ ] The PR title starts with "Fix #bugnum: ", followed by a short' +
+    ', clear summary of the changes. (If this PR fixes part of an issue, ' +
+    'prefix the title with "Fix part of #bugnum: ...".)\r\n' +
+    '- [ ] The linter/Karma presubmit checks have passed locally on ' +
+    'your machine.\r\n' +
+    '- [ ] "Allow edits from maintainers" is checked. (See [here]' +
+    '(https://help.github.com/en/github/collaborating-with-issues-and' +
+    '-pull-requests/allowing-changes-to-a-pull-request-branch-created-' +
+    'from-a-fork) for instructions on how to enable it.)\r\n  - This ' +
+    'lets reviewers restart your CircleCI tests for you.\r\n' +
+    "- [ ] The PR is made from a branch that's **not** called " +
+    '"develop".\r\n\r\n' +
     '## PR Pointers\r\n\r\n' +
     '- Oppiabot will ' +
     "notify you when you don't add a PR_CHANGELOG label.";
@@ -101,6 +126,7 @@ describe('Pull Request Template', () => {
     'lets reviewers restart your CircleCI tests for you.\r\n' +
     "- [ ] The PR is made from a branch that's **not** called " +
     '"develop".\r\n\r\n' +
+    '## Proof that changes are correct\r\n\r\n' +
     '## PR Pointers\r\n\r\n' +
     '- Oppiabot will ' +
     "notify you when you don't add a PR_CHANGELOG label.";
@@ -123,6 +149,30 @@ describe('Pull Request Template', () => {
     'lets reviewers restart your CircleCI tests for you.\r\n' +
     "- [ ] The PR is made from a branch that's **not** called " +
     '"develop".\r\n\r\n' +
+    '## Proof that changes are correct\r\n\r\n' +
+    '## PR Pointers\r\n\r\n' +
+    '- Oppiabot will ' +
+    "notify you when you don't add a PR_CHANGELOG label.";
+
+  const bodyWithExplanationAndChecklist =
+  ' ## Overview\r\n' +
+    '1. This PR fixes or fixes part of #[fill_in_number_here].\r\n' +
+    '2. This PR does the following: This PR does a bunch of things. ' +
+    '\r\n\r\n' +
+    '## Essential Checklist\r\n\r\n' +
+    '- [ ] The PR title starts with "Fix #bugnum: ", followed by a short' +
+    ', clear summary of the changes. (If this PR fixes part of an issue, ' +
+    'prefix the title with "Fix part of #bugnum: ...".)\r\n' +
+    '- [x] The linter/Karma presubmit checks have passed locally on ' +
+    'your machine.\r\n' +
+    '- [x] "Allow edits from maintainers" is checked. (See [here]' +
+    '(https://help.github.com/en/github/collaborating-with-issues-and' +
+    '-pull-requests/allowing-changes-to-a-pull-request-branch-created-' +
+    'from-a-fork) for instructions on how to enable it.)\r\n  - This ' +
+    'lets reviewers restart your CircleCI tests for you.\r\n' +
+    "- [ ] The PR is made from a branch that's **not** called " +
+    '"develop".\r\n\r\n' +
+    '## Proof that changes are correct\r\n\r\n' +
     '## PR Pointers\r\n\r\n' +
     '- Oppiabot will ' +
     "notify you when you don't add a PR_CHANGELOG label.";
@@ -145,6 +195,7 @@ describe('Pull Request Template', () => {
     'lets reviewers restart your CircleCI tests for you.\r\n' +
     "- [ ] The PR is made from a branch that's **not** called " +
     '"develop".\r\n\r\n' +
+    '## Proof that changes are correct\r\n\r\n' +
     '## PR Pointers\r\n\r\n' +
     '- Oppiabot will ' +
     "notify you when you don't add a PR_CHANGELOG label.";
@@ -167,9 +218,42 @@ describe('Pull Request Template', () => {
     'lets reviewers restart your CircleCI tests for you.\r\n' +
     "- [ ] The PR is made from a branch that's **not** called " +
     '"develop".\r\n\r\n' +
+    '## Proof that changes are correct\r\n\r\n' +
+    'https://user-images.githubusercontent.com/30050862/test.mp4' +
     '## PR Pointers\r\n\r\n' +
     '- Oppiabot will ' +
     "notify you when you don't add a PR_CHANGELOG label.";
+
+  const incompleteOverviewSection =
+    'The body of this PR is missing the required description, ' +
+    'please update the body with a description of what this PR does.';
+
+  const absentOverviewSection =
+    'The body of this PR is missing the overview section, please ' +
+    'update it to include the overview.';
+
+  const incompleteChecklistSection1 =
+    'The karma and linter checklist has not been checked, ' +
+    'please make sure to run the frontend tests and lint tests before ' +
+    'pushing. ';
+
+  const incompleteChecklistSection2 =
+    'The allow edits from maintainers checklist needs to ' +
+    'be ticked so that maintainers can rerun failed tests. ' +
+    'Endeavour to add this by ticking on the check box. ';
+
+  const absentChecklistSection =
+    'The body of this PR is missing the checklist section, please ' +
+    'update it to include the checklist.';
+
+  const incompleteProofOfChangesSection =
+    'The proof that changes are correct has not been provided, ' +
+    'please make sure to upload a image/video showing that the changes ' +
+    'are correct. ';
+
+  const absentProofOfChangesSection =
+    'The body of this PR is missing the proof that changes ' +
+    'are correct section, please update it to include the section.';
 
   /**
    * @type {import('probot').Probot} robot
@@ -240,13 +324,10 @@ describe('Pull Request Template', () => {
         body:
           'Hi @' +
           payloadData.payload.pull_request.user.login +
-          ', the body of this PR is missing the required description, ' +
-          'please update the body with a description of what this PR does.' +
-          '<br>Also, the karma and linter checklist has not been checked, ' +
-          'please make sure to run the frontend tests and lint tests before ' +
-          'pushing. The allow edits from maintainers checklist needs to ' +
-          'be ticked so that maintainers can rerun failed tests. Endeavour ' +
-          'to add this by ticking on the check box. Thanks!',
+          ', can you complete the following:' + '\n' +
+          '1. ' + incompleteOverviewSection + '\n' +
+          '2. ' + incompleteChecklistSection1 + incompleteChecklistSection2 +
+          '\n' + '3. ' + incompleteProofOfChangesSection + '\nThanks!',
       });
     });
 
@@ -281,13 +362,10 @@ describe('Pull Request Template', () => {
         body:
           'Hi @' +
           payloadData.payload.pull_request.user.login +
-          ', the body of this PR is missing the overview section, please ' +
-          'update it to include the overview.<br>' +
-          'Also, the karma and linter checklist has not been checked, ' +
-          'please make sure to run the frontend tests and lint tests before ' +
-          'pushing. The allow edits from maintainers checklist needs to ' +
-          'be ticked so that maintainers can rerun failed tests. Endeavour ' +
-          'to add this by ticking on the check box. Thanks!',
+          ', can you complete the following:' + '\n' +
+          '1. ' + absentOverviewSection + '\n' +
+          '2. ' + incompleteChecklistSection1 + incompleteChecklistSection2 +
+          '\n' + '3. ' + incompleteProofOfChangesSection + '\nThanks!',
       });
     });
 
@@ -320,12 +398,51 @@ describe('Pull Request Template', () => {
         repo: payloadData.payload.repository.name,
         owner: payloadData.payload.repository.owner.login,
         body:
-          'Hi @' +
-          payloadData.payload.pull_request.user.login +
-          ', the body of this PR is missing the required description, ' +
-          'please update the body with a description of what this PR does.' +
-          '<br>Also, the body of this PR is missing the checklist section, ' +
-          'please update it to include the checklist. Thanks!',
+        'Hi @' +
+        payloadData.payload.pull_request.user.login +
+        ', can you complete the following:' + '\n' +
+        '1. ' + incompleteOverviewSection + '\n' +
+        '2. ' + absentChecklistSection +
+        '\n' + '3. ' + incompleteProofOfChangesSection + '\nThanks!',
+      });
+    });
+
+    it('should assign PR author', () => {
+      expect(github.issues.addAssignees).toHaveBeenCalled();
+      expect(github.issues.addAssignees).toHaveBeenCalledWith({
+        issue_number: payloadData.payload.pull_request.number,
+        repo: payloadData.payload.repository.name,
+        owner: payloadData.payload.repository.owner.login,
+        assignees: [payloadData.payload.pull_request.user.login],
+      });
+    });
+  });
+
+  describe('when pull request with no Proof of changes section ' +
+  'is created', () => {
+    beforeEach(async () => {
+      payloadData.payload.pull_request.body = bodyWithNoProofOfChangesSection;
+      payloadData.payload.pull_request.maintainer_can_modify = false;
+      await robot.receive(payloadData);
+    });
+
+    it('should check the template', () => {
+      expect(checkPullRequestTemplateModule.checkTemplate).toHaveBeenCalled();
+    });
+
+    it('should ping PR author', () => {
+      expect(github.issues.createComment).toHaveBeenCalled();
+      expect(github.issues.createComment).toHaveBeenCalledWith({
+        issue_number: payloadData.payload.pull_request.number,
+        repo: payloadData.payload.repository.name,
+        owner: payloadData.payload.repository.owner.login,
+        body:
+        'Hi @' +
+        payloadData.payload.pull_request.user.login +
+        ', can you complete the following:' + '\n' +
+        '1. ' + incompleteOverviewSection + '\n' +
+        '2. ' + incompleteChecklistSection1 + incompleteChecklistSection2 +
+        '\n' + '3. ' + absentProofOfChangesSection + '\nThanks!',
       });
     });
 
@@ -358,13 +475,11 @@ describe('Pull Request Template', () => {
         repo: payloadData.payload.repository.name,
         owner: payloadData.payload.repository.owner.login,
         body:
-          'Hi @' +
-          payloadData.payload.pull_request.user.login +
-          ', the karma and linter checklist has not been checked, ' +
-          'please make sure to run the frontend tests and lint tests before ' +
-          'pushing. The allow edits from maintainers checklist needs to ' +
-          'be ticked so that maintainers can rerun failed tests. Endeavour ' +
-          'to add this by ticking on the check box. Thanks!',
+        'Hi @' +
+        payloadData.payload.pull_request.user.login +
+        ', can you complete the following:' + '\n' +
+        '1. ' + incompleteChecklistSection1 + incompleteChecklistSection2 +
+        '\n' + '2. ' + incompleteProofOfChangesSection + '\nThanks!',
       });
     });
 
@@ -398,10 +513,48 @@ describe('Pull Request Template', () => {
           repo: payloadData.payload.repository.name,
           owner: payloadData.payload.repository.owner.login,
           body:
-            'Hi @' + payloadData.payload.pull_request.user.login +
-            ', the allow edits from maintainers checklist needs to ' +
-            'be ticked so that maintainers can rerun failed tests. Endeavour ' +
-            'to add this by ticking on the check box. Thanks!',
+          'Hi @' +
+          payloadData.payload.pull_request.user.login +
+          ', can you complete the following:' + '\n' +
+          '1. ' + incompleteChecklistSection2 +
+          '\n' + '2. ' + incompleteProofOfChangesSection + '\nThanks!',
+        });
+      });
+
+      it('should assign PR author', () => {
+        expect(github.issues.addAssignees).toHaveBeenCalled();
+        expect(github.issues.addAssignees).toHaveBeenCalledWith({
+          issue_number: payloadData.payload.pull_request.number,
+          repo: payloadData.payload.repository.name,
+          owner: payloadData.payload.repository.owner.login,
+          assignees: [payloadData.payload.pull_request.user.login],
+        });
+      });
+    });
+
+  describe('when pull request contains description and filled Checklist',
+    () => {
+      beforeEach(async () => {
+        payloadData.payload.pull_request.body = bodyWithExplanationAndChecklist;
+        payloadData.payload.pull_request.maintainer_can_modify = false;
+        await robot.receive(payloadData);
+      });
+
+      it('should check the template', () => {
+        expect(checkPullRequestTemplateModule.checkTemplate).toHaveBeenCalled();
+      });
+
+      it('should ping PR author', () => {
+        expect(github.issues.createComment).toHaveBeenCalled();
+        expect(github.issues.createComment).toHaveBeenCalledWith({
+          issue_number: payloadData.payload.pull_request.number,
+          repo: payloadData.payload.repository.name,
+          owner: payloadData.payload.repository.owner.login,
+          body:
+          'Hi @' +
+          payloadData.payload.pull_request.user.login +
+          ', can you complete the following:' + '\n' +
+          '1. ' + incompleteProofOfChangesSection + '\nThanks!',
         });
       });
 
@@ -436,10 +589,11 @@ describe('Pull Request Template', () => {
           repo: payloadData.payload.repository.name,
           owner: payloadData.payload.repository.owner.login,
           body:
-            'Hi @' + payloadData.payload.pull_request.user.login +
-            ', the body of this PR is missing the required description, ' +
-            'please update the body with a description of what this PR does. ' +
-            'Thanks!',
+          'Hi @' +
+          payloadData.payload.pull_request.user.login +
+          ', can you complete the following:' + '\n' +
+          '1. ' + incompleteOverviewSection +
+          '\n' + '2. ' + incompleteProofOfChangesSection + '\nThanks!',
         });
       });
 
@@ -494,11 +648,11 @@ describe('Pull Request Template', () => {
         repo: payloadData.payload.repository.name,
         owner: payloadData.payload.repository.owner.login,
         body:
-          'Hi @' +
-          payloadData.payload.pull_request.user.login +
-          ', the karma and linter checklist has not been checked, ' +
-          'please make sure to run the frontend tests and lint tests ' +
-          'before pushing. Thanks!',
+        'Hi @' +
+        payloadData.payload.pull_request.user.login +
+        ', can you complete the following:' + '\n' +
+        '1. ' + incompleteChecklistSection1 +
+        '\n' + '2. ' + incompleteProofOfChangesSection + '\nThanks!',
       });
     });
   });
