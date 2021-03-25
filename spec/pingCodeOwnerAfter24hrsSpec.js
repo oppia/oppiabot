b5a2f25c95050393b84fcfb0c416f7fea6eb913a
Naman-1234 <namankalrabhiwani54@gmail.com>
2021-03-25 23:00:32 +0530

    Tests Written for Js file

---
+++ spec/pingCodeOwnerAfter24hrsSpec.js
@@ -0,0 +1,98 @@
+// Copyright 2020 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+require('dotenv').config();
+const { createProbot } = require('probot');
+//Following plugin is actual app in index.jd
+const oppiaBot = require('../index');
+const scheduler = require('../lib/scheduler');
+const pushPayload = require('../fixtures/push.json');
+const pullRequestPayload = require('../fixtures/pullRequestPayload.json');
+const pingCodeOwnerModule = require('../lib/pingCodeOwnerAfter24hrs');
+const checkBranchPushModule = require('../lib/checkBranchPush');
+
+describe('Should Ping after 24 hrs without review in PR', ()=> {
+  /**
+    * @type {import('probot').Probot} robot
+    */
+  let robot;
+
+  /**
+    * @type {import('probot').Octokit} github
+    */
+  let github;
+
+  /**
+    * @type {import('probot').Application} app
+    */
+  let app;
+  beforeEach(()=>{
+    spyOn(scheduler, 'createScheduler').and.callFake(()=>{});
+
+    github = {
+      issues: {
+        createComment: jasmine.createSpy('createComment').and.returnValue({}),
+        update: jasmine.createSpy('update').and.resolveTo({}),
+      },
+    };
+
+    robot = createProbot({
+      id: 1,
+      cert: 'test',
+      githubToken: 'test',
+    }
+    );
+    app = robot.load(oppiaBot);
+    spyOn(app, 'auth').and.resolveTo(github);
+    spyOn(pingCodeOwnerModule, 'checkAnyReviewRequiredPr').and.callThrough();
+  });
+
+  //BeforeEach Ends here
+
+  // From here Starting Describe suite to check
+  describe('Its been more than 24 Hrs since' +
+  'last review and there is no Code Review till now ', ()=>{
+    beforeEach(async () => {
+      await robot.receive(pushPayload);
+    });
+
+    it('should comment on pull request', ()=>{
+      expect(github.issues.createComment).toHaveBeenCalled();
+    });
+
+    it('should not close the pull request', () => {
+      expect(github.issues.update).not.toHaveBeenCalled();
+    });
+
+    it('should check for force push', () => {
+      expect(checkBranchPushModule.handleForcePush).toHaveBeenCalled();
+    });
+  });
+
+  describe('Less than 24 Hrs', ()=>{
+    // In this beforeEach is not Required as we don't require any
+    // data before Performing these tests
+    it('should not close the pull request', () => {
+      expect(github.issues.update).not.toHaveBeenCalled();
+    });
+
+    it('should check for force push', () => {
+      expect(checkBranchPushModule.handleForcePush).toHaveBeenCalled();
+    });
+
+    it('should not comment on pull request', ()=>{
+      expect(github.issues.createComment).not.toHaveBeenCalled();
+    });
+  });
+});
