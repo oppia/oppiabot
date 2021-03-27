b1fb123340d1e09b1f0a80589736e7dcbc462688
Praneeth <praneethg2001@gmail.com>
2021-03-27 12:15:40 +0530

    added cla_check files

---
+++ actions1/cla_check.js
@@ -0,0 +1,62 @@
+const core = require('@actions/core');
+const { context, GitHub } = require('@actions/github');
+const { google } = require('googleapis');
+
+const SHEETS_TOKEN = process.env.SHEETS_TOKEN;
+const CREDENTIALS = JSON.parse(process.env.SHEETS_CRED);
+const SPREADSHEET_ID = '1naQC7iEfnro5iOjTFEn7iPCxNMPaPa4YnIddjT5CTM8';
+const RANGE = 'Usernames';
+const PR_AUTHOR = context.payload.pull_request.user.login;
+// const PR_NUMBER = context.payload.pull_request.number;
+// const LINK_RESULT = (
+//   'https://github.com/oppia/oppia/wiki' +
+//   '/Contributing-code-to-Oppia#setting-things-up')
+
+authorize(claCheck);
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} CREDENTIALS The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+var authorize = function(callback) {
+  const { clinetSecret, clinetId, redirectUris } = CREDENTIALS.web;
+  const oAuth2Client = new google.auth.OAuth2(
+    clinetId,
+    clinetSecret,
+    redirectUris[0]
+  );
+  oAuth2Client.setCredentials(JSON.parse(SHEETS_TOKEN));
+  callback(oAuth2Client);
+};
+
+/**
+ * Prints the names and majors of students in a sample spreadsheet:
+ * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+ */
+var claCheck = function(auth) {
+  const sheets = google.sheets({ version: 'v4', auth });
+  sheets.spreadsheets.values.get(
+    {
+      spreadsheetId: SPREADSHEET_ID,
+      range: RANGE,
+    },
+    (err, res) => {
+      if (err) {
+        return console.log('The API returned an error: ' + err);
+      }
+      const rows = res.data.values;
+      const flatRows = [].concat.apply([], rows);
+      if (rows.length) {
+        console.log('Checking if ', PR_AUTHOR, ' has signed the CLA');
+        const isSign = flatRows.includes(PR_AUTHOR);
+        if (!isSign) {
+          core.setFailed(PR_AUTHOR + ' has not signed the CLA');
+        }
+      } else {
+        console.log('No data found.');
+      }
+    }
+  );
+};
