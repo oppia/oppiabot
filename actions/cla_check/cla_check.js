46b08c3c447a0744f9310a9166bc56149fa53615
Praneeth <praneethg2001@gmail.com>
2021-03-28 04:18:16 +0530

    debug-6

--- actions/cla_check/cla_check.js
+++ actions/cla_check/cla_check.js
@@ -88,14 +88,14 @@ const checkSheet = async (auth) => {
       }
       const rows = res.data.values;
       const flatRows = [].concat.apply([], rows);
-      if (rows.length) {
+      if (!rows || rows.length === 0) {
+        core.setFailed('No data found.');
+      } else {
         console.log('Checking if ', PR_AUTHOR, ' has signed the CLA');
         console.log('🚀gp201 ~ checkSheet ~ flatRows.includes(PR_AUTHOR)',
           flatRows.includes(PR_AUTHOR));
         const hasUserSignedCla = flatRows.includes(PR_AUTHOR);
         generateOutput(hasUserSignedCla);
-      } else {
-        core.setFailed('No data found.');
       }
     }
   );
