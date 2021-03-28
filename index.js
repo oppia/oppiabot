bc3305d382f1343c1fa904b0ae2f4564c8d383bc
Naman-1234 <namankalrabhiwani54@gmail.com>
2021-03-28 18:46:08 +0530

    Changes Requested Done

--- index.js
+++ index.js
@@ -145,8 +145,7 @@ const runChecks = async (context, checkEvent) => {
             callable.push(...[
               periodicCheckModule.ensureAllPullRequestsAreAssigned(context),
               periodicCheckModule.ensureAllIssuesHaveProjects(context),
-              staleBuildModule.checkAndTagPRsWithOldBuilds(context),
-              checkPullRequestReviewModule.handleLabels(context)
+              staleBuildModule.checkAndTagPRsWithOldBuilds(context)
             ]);
             break;
           case constants.respondToReviewCheck:
