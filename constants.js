d61e451c97b97d29ffbc4d4edca19552bccf1200
Praneeth <praneethg2001@gmail.com>
2021-05-13 15:01:30 +0530

    changed constant.js test changes

--- constants.js
+++ constants.js
@@ -130,6 +130,10 @@ const checksWhitelist = {
     [pullRequestReviewEvent]: [pullRequestReviewCheck],
     [issueCommentCreatedEvent]: [respondToReviewCheck],
   },
+  'test-github-actions': {
+    [openEventGithubActions]: [claCheckGithubAction],
+    [reopenEventGithubActions]: [claCheckGithubAction],
+  },
 };
 
 const blacklistedAuthors = ['translatewiki'];
