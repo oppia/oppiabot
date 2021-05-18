2d02f3267d758aee6c5d20f3fdf3cc807b94807e
mridul-netizen <mridulsetia18@gmail.com>
2021-05-18 06:53:53 +0530

    Fix part of #174: Add test to ensure that bot doesn't gets triggred with "@{{username}} PTAL" comment (#191)
    * added-test
    
    * updated
    
    * updated
    
    * test-updated
    
    * updated
    
    * updated
    
    * nits

--- spec/checkPullRequestReviewSpec.js
+++ spec/checkPullRequestReviewSpec.js
@@ -1587,5 +1587,24 @@ describe('Pull Request Review Module', () => {
         });
       }
     );
+
+    describe('when PTAL template found in the comment', () => {
+      const initialCommentBody = commentPayloadData.payload.comment.body;
+      beforeAll(() => {
+        commentPayloadData.payload.comment.body = (
+          '{{Question/comment}} @{{reviewer_username}} PTAL');
+      });
+      beforeEach(async () => {
+        await robot.receive(commentPayloadData);
+      });
+
+      it('should not assign anyone to the PR', () => {
+        expect(github.issues.createComment).not.toHaveBeenCalled();
+      });
+
+      afterAll(() => {
+        commentPayloadData.payload.comment.body = initialCommentBody;
+      });
+    });
   });
 });
