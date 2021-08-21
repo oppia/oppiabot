db73557e9385bd3cd3174b2dae3925b6a517b221
Praneeth <praneethg2001@gmail.com>
2021-08-21 19:40:01 +0530

    testing

--- actions/src/dispatcher.js
+++ actions/src/dispatcher.js
@@ -31,8 +31,10 @@ module.exports = {
     const repoName = context.payload.repository.name.toLowerCase();
     const checksWhitelist = constants.getChecksWhitelist();
     if (Object.prototype.hasOwnProperty.call(checksWhitelist, repoName)) {
+      core.info('ran1');
       const checks = checksWhitelist[repoName];
       if ((Object.prototype.hasOwnProperty.call(checks, checkEvent))) {
+        core.info('ran1');
         const checkList = checks[checkEvent];
         for (var i = 0; i < checkList.length; i++) {
           switch (checkList[i]) {
