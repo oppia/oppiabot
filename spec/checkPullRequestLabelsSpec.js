df9b14d0a16632f2e1f81ae22a098c993100515d
Ankita Saxena <ankitasonu24@gmail.com>
2022-04-13 11:58:29 +0100

    Fix syntax

--- spec/checkPullRequestLabelsSpec.js
+++ spec/checkPullRequestLabelsSpec.js
@@ -687,7 +687,7 @@ describe('Pull Request Label Check', () => {
         body:
           'Hi, @oppia/release-coordinators flagging this pull request for ' +
           'for your attention since this is labelled as a hotfix PR. ' +
-          'Please ensure that you add the \'PR: for current release\' ' +
+          'Please ensure that you add the "PR: for current release" ' +
           'label if the next release is in progress. Thanks!'
         number: payloadData.payload.pull_request.number,
         owner: payloadData.payload.repository.owner.login,
